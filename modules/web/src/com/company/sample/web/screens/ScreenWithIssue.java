f83922711db133dde2f509530350fe812451df0d
rushan <rushan.developer@gmail.com>
2017-07-25 09:43:01 +0500

    sample of issue when setting footer value with aggregation

---
+++ modules/web/src/com/company/sample/web/screens/ScreenWithIssue.java
@@ -0,0 +1,26 @@
+package com.company.sample.web.screens;
+
+import com.haulmont.cuba.security.entity.User;
+import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import com.haulmont.chile.core.model.MetaPropertyPath;
+import javax.inject.Inject;
+import java.util.Map;
+import java.util.UUID;
+
+public class ScreenWithIssue extends AbstractWindow {
+    @Inject
+    private Table<User> sampleTable;
+    @Inject
+    private CollectionDatasource<User, UUID> usersDs;
+    
+    @Override
+    public void init(Map<String, Object> params) {
+        MetaPropertyPath userVersionPropertyPath = 
+                usersDs.getMetaClass().getPropertyPath("login");
+        sampleTable.unwrap(com.vaadin.ui.Table.class)
+                .setFooterVisible(true);
+        sampleTable.unwrap(com.vaadin.ui.Table.class)
+                .setColumnFooter(userVersionPropertyPath, "constant footer value");
+    }
+}
